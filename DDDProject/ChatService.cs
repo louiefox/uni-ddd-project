8c17cb0623f76acf40c5738f5107243a17a5efb4
Brickwall <louie.waterfall@hotmail.co.uk>
2023-02-28 15:43:47 +0000

    Chat stuff

---
+++ DDDProject/ChatService.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DDDProject
+{
+    public class SocietyChatHub : Hub
+    {
+        public const string HubUrl = "/chat";
+
+        public async Task Broadcast(string username, string message)
+        {
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(e);
+        }
+    }
+}
--- DDDProject/Startup.cs
+++ DDDProject/Startup.cs
@@ -54,6 +54,7 @@ namespace DDDProject
             {
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapHub<SocietyChatHub>("/chat");
             });
         }
     }
