8b37380b0b9fdc63c654bd1f6c1c394d5f22306d
Brickwall <louie.waterfall@hotmail.co.uk>
2023-03-28 02:03:39 +0100

    Poopy login stuff

---
+++ DDDProject/Shared/LoginService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DDDProject.Utils
+{
+    public static class LoginService
+    {
+        public static bool IsLoggedIn = false;
+    }
+}
