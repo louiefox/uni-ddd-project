fe8aec9491a5d9b50b0480b6b6fa0f29c5b36b41
Brickwall <louie.waterfall@hotmail.co.uk>
2023-04-20 22:46:14 +0100

    Fix some login stuff

--- DDDProject/Shared/ClientLoginService.cs
+++ DDDProject/Shared/ClientLoginService.cs
@@ -9,14 +9,20 @@ namespace DDDProject.Utils
 {
     public static class ClientLoginService
     {
-        public static bool IsLoggedIn = false;
+        public static bool IsLoggedIn { 
+            get {
+                return LoginToken != "";
+            } 
+        }
+
+        public static string LoginToken = "";
         public static string Username = "";
         public static string Fullname = "";
 
         public static async void FetchUserInfo()
         {
             LoginService loginService = new();
-            Fullname = await loginService.RequestUserInfo(Username);
+            Fullname = await loginService.RequestUserInfo(LoginToken);
         }
     }
 }
